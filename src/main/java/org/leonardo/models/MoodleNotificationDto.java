1d841f570fe2cedb0f1498841ab7143f7668b77d
TChrisostomo <t.chrisostomo@reply.it>
2023-06-29 20:57:16 +0200

    Initial commit

---
+++ src/main/java/org/leonardo/models/MoodleNotificationDto.java
@@ -0,0 +1,340 @@
+package org.leonardo.models;
+
+public class MoodleNotificationDto {
+
+    private String username ;
+    private String notificationtype;
+    private Long retention;
+    private Long timestart;
+    private Long timeend;
+    private String courseid;
+    private String sessionid;
+    private Long sessiondate;
+    private String certificate;
+    private Long inserttime;
+    private Long completiondate;
+    private Long total_duration;
+    private Long expectedcompletion;
+    private String usergroup;
+    private String oldusergroup;
+    private Long count_courses_completed;
+    private Long month;
+    private String managerof;
+    private String preferrednotificationchannels;
+    private String email;
+    private String preferredlang;
+    private String sender_system;
+    private String firstname;
+    private String lastname;
+    private String timezone;
+    private String courseTitle;
+    private Long coursedate;
+    private Long coursetime;
+    private String lpid;
+    private String cohortid;
+    private String moduleid;
+    //ecommerce
+    private String object;
+    private String token;
+    private String mailuser;
+    private Long tokentimeactivated;
+    private Long tokentimeended;
+    private Long tokenduration;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNotificationtype() {
+        return notificationtype;
+    }
+
+    public void setNotificationtype(String notificationtype) {
+        this.notificationtype = notificationtype;
+    }
+
+    public Long getRetention() {
+        return retention;
+    }
+
+    public void setRetention(Long retention) {
+        this.retention = retention;
+    }
+
+    public Long getTimestart() {
+        return timestart;
+    }
+
+    public void setTimestart(Long timestart) {
+        this.timestart = timestart;
+    }
+
+    public Long getTimeend() {
+        return timeend;
+    }
+
+    public void setTimeend(Long timeend) {
+        this.timeend = timeend;
+    }
+
+    public String getCourseid() {
+        return courseid;
+    }
+
+    public void setCourseid(String courseid) {
+        this.courseid = courseid;
+    }
+
+    public String getSessionid() {
+        return sessionid;
+    }
+
+    public void setSessionid(String sessionid) {
+        this.sessionid = sessionid;
+    }
+
+    public Long getSessiondate() {
+        return sessiondate;
+    }
+
+    public void setSessiondate(Long sessiondate) {
+        this.sessiondate = sessiondate;
+    }
+
+    public String getCertificate() {
+        return certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        this.certificate = certificate;
+    }
+
+    public Long getInserttime() {
+        return inserttime;
+    }
+
+    public void setInserttime(Long inserttime) {
+        this.inserttime = inserttime;
+    }
+
+    public Long getCompletiondate() {
+        return completiondate;
+    }
+
+    public void setCompletiondate(Long completiondate) {
+        this.completiondate = completiondate;
+    }
+
+    public Long getTotal_duration() {
+        return total_duration;
+    }
+
+    public void setTotal_duration(Long total_duration) {
+        this.total_duration = total_duration;
+    }
+
+    public Long getExpectedcompletion() {
+        return expectedcompletion;
+    }
+
+    public void setExpectedcompletion(Long expectedcompletion) {
+        this.expectedcompletion = expectedcompletion;
+    }
+
+    public String getUsergroup() {
+        return usergroup;
+    }
+
+    public void setUsergroup(String usergroup) {
+        this.usergroup = usergroup;
+    }
+
+    public String getOldusergroup() {
+        return oldusergroup;
+    }
+
+    public void setOldusergroup(String oldusergroup) {
+        this.oldusergroup = oldusergroup;
+    }
+
+    public Long getCount_courses_completed() {
+        return count_courses_completed;
+    }
+
+    public void setCount_courses_completed(Long count_courses_completed) {
+        this.count_courses_completed = count_courses_completed;
+    }
+
+    public Long getMonth() {
+        return month;
+    }
+
+    public void setMonth(Long month) {
+        this.month = month;
+    }
+
+    public String getManagerof() {
+        return managerof;
+    }
+
+    public void setManagerof(String managerof) {
+        this.managerof = managerof;
+    }
+
+    public String getPreferrednotificationchannels() {
+        return preferrednotificationchannels;
+    }
+
+    public void setPreferrednotificationchannels(String preferrednotificationchannels) {
+        this.preferrednotificationchannels = preferrednotificationchannels;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPreferredlang() {
+        return preferredlang;
+    }
+
+    public void setPreferredlang(String preferredlang) {
+        this.preferredlang = preferredlang;
+    }
+
+    public String getSender_system() {
+        return sender_system;
+    }
+
+    public void setSender_system(String sender_system) {
+        this.sender_system = sender_system;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public Long getCoursedate() {
+        return coursedate;
+    }
+
+    public void setCoursedate(Long coursedate) {
+        this.coursedate = coursedate;
+    }
+
+    public Long getCoursetime() {
+        return coursetime;
+    }
+
+    public void setCoursetime(Long coursetime) {
+        this.coursetime = coursetime;
+    }
+
+    public String getLpid() {
+        return lpid;
+    }
+
+    public void setLpid(String lpid) {
+        this.lpid = lpid;
+    }
+
+    public String getCohortid() {
+        return cohortid;
+    }
+
+    public void setCohortid(String cohortid) {
+        this.cohortid = cohortid;
+    }
+
+    public String getModuleid() {
+        return moduleid;
+    }
+
+    public void setModuleid(String moduleid) {
+        this.moduleid = moduleid;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getMailuser() {
+        return mailuser;
+    }
+
+    public void setMailuser(String mailuser) {
+        this.mailuser = mailuser;
+    }
+
+    public Long getTokentimeactivated() {
+        return tokentimeactivated;
+    }
+
+    public void setTokentimeactivated(Long tokentimeactivated) {
+        this.tokentimeactivated = tokentimeactivated;
+    }
+
+    public Long getTokentimeended() {
+        return tokentimeended;
+    }
+
+    public void setTokentimeended(Long tokentimeended) {
+        this.tokentimeended = tokentimeended;
+    }
+
+    public Long getTokenduration() {
+        return tokenduration;
+    }
+
+    public void setTokenduration(Long tokenduration) {
+        this.tokenduration = tokenduration;
+    }
+}
